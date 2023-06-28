93acf4013617822c61d1595208bd744313765d7c
NeuDegold <gitlos76@gmail.com>
2023-06-28 13:42:21 +0300

    update main file

--- main.dart
+++ main.dart
@@ -1,1 +1,9 @@
-"main Dart file " 
+import 'dart:ma';
+void main() {
+//=============================
+  List name = ["amer","degold","nour","osama"];
+var num = name.where((element) => element.length == 6);
+print(num);
+
+//degold
+}
---
+++ second.dart
@@ -0,0 +1,3 @@
+ String myname (int x,String n){
+  return ("add num $x and my name is: $n");
+}
